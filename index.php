fb705b3131f3110c36446f08c3084fd300a4d80a
AkitoHorizon <jojo42424242424242@gmail.com>
2026-01-13 15:10:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Mécanique Anciennes du Haut-Lignon</title>
 </head>
 <body>
     
